7c2f75503017efe4c736c4acd89a0936e0ed13a2
Faria-Binte-Kader <fariabintekader67@gmail.com>
2021-04-25 04:27:09 +0600

    login page added

--- app/src/main/java/com/example/note_ally/login.java
+++ app/src/main/java/com/example/note_ally/login.java
@@ -79,7 +79,11 @@ public class login extends AppCompatActivity implements AdapterView.OnItemSelect
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                               // startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                Intent intent = new Intent(login.this, MainActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                                finish();
                             } else {
                                 Toast.makeText(login.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
